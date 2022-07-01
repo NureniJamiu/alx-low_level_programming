37867d52b2020c9b1cfb48e5d7ffe80e3362d2f6
Dhulnurein <nurenijamiu951@gmail.com>
2022-07-01 20:32:36 +0100

    add script 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* 
+/*
  * main - This program prints all the alphabets
  * in lowercase excluding the q and e characters
  * and explictly uses the putchar() function
@@ -11,7 +11,7 @@ int main(void)
 	char letters;
 
 	for (letters = 'a'; letters <= 'z'; letters++)
-		if (letters !='e' && letters !='q')
+		if (letters ! = 'e' && letters ! = 'q')
 			putchar(letters);
 	putchar('\n');
 	return (0);
