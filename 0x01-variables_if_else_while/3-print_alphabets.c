6b5a02be4802967790c265937081a49811556689
Dhulnurein <nurenijamiu951@gmail.com>
2022-07-01 12:49:24 +0100

    add script 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char letters;
+
 	for (letters = 'a'; letters <= 'z'; letters++)
 		putchar(letters);
 	for (letters = 'A'; letters < 'Z'; letters++)
-                putchar(letters);
+		putchar(letters);
 	putchar('\n');
 	return (0);
 }
