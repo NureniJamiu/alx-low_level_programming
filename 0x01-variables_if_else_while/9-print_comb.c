ce3b326fc92f2b2f676b3c25f267800cfa3aa210
Dhulnurein <nurenijamiu951@gmail.com>
2022-07-01 22:58:38 +0100

    Update script 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
+ * main - prints all single digit numbers of
+ * base10 starting from 0,
  * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int fmt = ", ";
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		putchar(fmt);
+		putchar(', ');
 	}
 	putchar('\n');
 	return (0);
