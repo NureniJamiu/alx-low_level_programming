24bb775e30c0454d20e525915c9ed5a9b17baa38
Dhulnurein <nurenijamiu951@gmail.com>
2022-07-01 20:33:41 +0100

    add script 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 	char letters;
 
 	for (letters = 'a'; letters <= 'z'; letters++)
-		if (letters ! = 'e' && letters ! = 'q')
+		if (letters !='e' && letters !='q')
 			putchar(letters);
 	putchar('\n');
 	return (0);
